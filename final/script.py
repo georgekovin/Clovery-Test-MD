d32ef4f7250f71d8e471ebb857b8c83ce90e51f9
georgekovin <108282258+georgekovin@users.noreply.github.com>
2024-10-23 20:36:33 +0300

    final commit

--- final/script.py
+++ final/script.py
@@ -3,12 +3,15 @@ import pickle as pkl
 
 
 if __name__ == '__main__':  
+    # читаем датасет с открытыми данными и идентификаторы объектов
     data = pd.read_csv('data.csv')
     ids = pd.read_parquet('test.parquet')['id']
     
+    # расконсервируем модель
     with open('model.pkl', 'rb') as model_f:
         model = pkl.load(model_f)
     
+    # вычисляем вероятности и создаем submission
     score = model.predict_proba(data)[:, 1]
     
     submission = pd.DataFrame({
